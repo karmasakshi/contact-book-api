b5939600c380817ad09c32a86c493e1be49301af
Karmasakshi Goyal <karmasakshi@Karmasakshis-MacBook-Air.local>
2017-09-02 23:58:46 +0530

    Add search API.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ A simple searchable contact-book API based on Sails.
 
 ## Features
 
-Use any REST client to CRUD `/contacts`. This app uses Sails Disk as the database. Configuration can be added to use any other database server like MySQL or MongoDB. CORS rules have been purposely relaxed, not suited for production.
+Use any REST client to CRUD `/contacts`. This app uses Sails Disk as the database. Configuration can be added to use any other database server like MySQL or MongoDB. CORS rules have been purposely relaxed, not suited for production. `/contacts/count` returns count. `/contacts/search` returns search results.
 
 ## Tests
 
--- api/controllers/ContactController.js
+++ api/controllers/ContactController.js
@@ -13,11 +13,53 @@ module.exports = {
 
       if (error) {
 
-        return response.error(err);
+        return response.serverError(error);
 
       } else {
 
         return response.json(count);
+
+      }
+
+    });
+
+  },
+
+  'search': function (request, response) {
+
+    Contact.find({
+      or: [
+        { email: { contains: request.query.searchKey } },
+        { firstName: { contains: request.query.searchKey } },
+        { lastName: { contains: request.query.searchKey } },
+        { phone: { contains: request.query.searchKey } }
+      ]
+    }).exec(function (error, contacts) {
+
+      if (error) {
+
+        return response.serverError(error);
+
+      } else {
+
+        // Remove duplicates. Can be replaced by a native query of a more advanced database server.
+        var contactsIndices = [];
+        var result = [];
+
+        contacts.forEach(function (contact) {
+
+          if (contactsIndices.indexOf(contact.id) === -1) {
+
+            contactsIndices.push(contact.id);
+
+            result.push(contact);
+
+          }
+
+        }, this);
+
+        // Respond.
+        return response.json(result);
 
       }
 
