aedf19a4c5191c276c217f07b104a1e812055503
Karmasakshi Goyal <karmasakshi@Karmasakshis-MacBook-Air.local>
2017-09-02 17:21:51 +0530

    Update models and configurations.

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -9,6 +9,14 @@ module.exports = {
 
   attributes: {
 
-  }
-};
+    firstName: {
+      type: 'string'
+    },
 
+    lastName: {
+      type: 'string'
+    }
+
+  }
+
+};
--- api/models/Group.js
+++ api/models/Group.js
@@ -9,6 +9,10 @@ module.exports = {
 
   attributes: {
 
-  }
-};
+    name: {
+      type: 'string'
+    }
 
+  }
+
+};
